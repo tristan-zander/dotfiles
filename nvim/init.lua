a3faf556508a4f9d50b4500395b5bd56f7ff0ccf
Tristan Zander <tristan.zander@ncino.com>
2024-01-03 16:11:50 -0500

    Basic LF and Colorscheme

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+-- Set the leader key
+vim.g.mapleader = " "
+
+-- Download Lazy, if it doesn't exist
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup Lazy with necessary plugins.
+require("lazy").setup("plugins")
