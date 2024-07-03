54bd2bbb7b990e94305595b9398f0f495424a66a
Tristan Zander <tristan.zander@ncino.com>
2024-07-03 14:27:36 -0400

    better setup lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,11 @@
 -- Download Lazy, if it doesn't exist
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,5 +15,3 @@ vim.g.mapleader = ' '
 -- Setup Lazy with necessary plugins.
 require("lazy").setup("plugins")
 
--- Add keybinds
-require('keybinds')
---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        opts = {
+            --            mapping = {
+            --                 ["<C-Space>"] = cmp.mapping.complete() -- Replace with whatever you want
+            --           }
+        }
+    }
+}
+
