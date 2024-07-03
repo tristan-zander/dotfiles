d577c10fdadadafa62779077b535b6edc9f40e88
Tristan Zander <tristan.zander@ncino.com>
2024-07-03 14:34:05 -0400

    Setup which key

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,10 +5,14 @@ return {
         dependencies = {'nvim-lua/plenary.nvim'},
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files,
+                           {desc = '[F]ind [F]iles'})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+                           {desc = '[F]ind [G]rep'})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers,
+                           {desc = '[F]ind [B]uffer'})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+                           {desc = '[F]ind [H]elp'})
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api
