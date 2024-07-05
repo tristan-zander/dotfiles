e2dc22de33eb2da3233a9f68ce734f6b81884e86
Tristan Zander <tristan.zander@ncino.com>
2024-07-05 11:20:58 -0400

    add autoformatting configuration

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -42,8 +42,60 @@ return {
 
             conform.setup({
                 formatters_by_ft = {lua = {"lua-format"}},
-                format_on_save = {timeout_ms = 500, lsp_fallback = true}
+                format_on_save = function(bufnr)
+                    -- Disable with a global or buffer-local variable
+                    if vim.g.disable_autoformat or
+                        vim.b[bufnr].disable_autoformat then
+                        return
+                    end
+                    return {timeout_ms = 500, lsp_format = "fallback"}
+
+                end
             })
+
+            -- Create user commands to toggle auto formatting
+            vim.api.nvim_create_user_command("FormatDisable", function(args)
+                if args.bang then
+                    -- FormatDisable! will disable formatting just for this buffer
+                    vim.b.disable_autoformat = true
+                else
+                    vim.g.disable_autoformat = true
+                end
+            end, {desc = "Disable autoformat-on-save", bang = true})
+            vim.api.nvim_create_user_command("FormatEnable", function(args)
+                if args.bang then
+                    -- FormatEnable! will enable formatting just for this buffer
+                    vim.b.disable_autoformat = false
+                else
+                    vim.g.disable_autoformat = false
+                end
+            end, {desc = "Re-enable autoformat-on-save", bang = true})
+
+            -- toggle buffer formatting
+            vim.keymap.set('n', '<leader>tfb', function()
+                if vim.b == nil then return end
+
+                if vim.b.disable_autoformat == false then
+                    vim.api.nvim_command("FormatDisable!")
+                    vim.print("disabled buffer autoformat")
+                else
+                    vim.api.nvim_command("FormatEnable!")
+                    vim.print("enabled buffer autoformat")
+                end
+            end, {desc = '[T]oggle [F]ormatter ([B]uffer)'})
+
+            -- toggle global formatting
+            vim.keymap.set('n', '<leader>tfg', function()
+                if vim.g.disable_autoformat == false then
+                    vim.api.nvim_command("FormatDisable")
+                    vim.print("disabled global autoformat")
+                else
+                    vim.api.nvim_command("FormatEnable")
+                    vim.print("enabled global autoformat")
+                end
+            end, {desc = '[T]oggle [F]ormatter ([B]uffer)'})
+
         end
+
     }
 }
