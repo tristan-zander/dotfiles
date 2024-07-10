d8902380e0efbfc1972c611e65376628e065f641
Tristan Zander <tristan.zander@ncino.com>
2024-07-10 16:22:19 -0400

    add alacritty and LF configs

--- stow/.config/nvim/lua/plugins/telescope.lua
+++ stow/.config/nvim/lua/plugins/telescope.lua
@@ -5,8 +5,10 @@ return {
         dependencies = {'nvim-lua/plenary.nvim'},
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files,
-                           {desc = '[F]ind [F]iles'})
+            vim.keymap.set('n', '<leader>ff',
+                           function()
+                builtin.find_files({hidden = true})
+            end, {desc = '[F]ind [F]iles'})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep,
                            {desc = '[F]ind [G]rep'})
             vim.keymap.set('n', '<leader>fb', builtin.buffers,
